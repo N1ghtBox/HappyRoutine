77e388851a64c566b663a26ac265fde498203c40
Dawid <d.witczak@a-soft.pl>
2022-06-06 20:55:46 +0200

    init
    Initial commit plus started working on auth

---
+++ app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from "next-auth/providers/google";
+import prisma from '../../../lib/prisma';
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  }
+};
