bd29277945eb85a59d0322f4ccf02da57796fa8f
Dawid <d.witczak@a-soft.pl>
2022-06-26 16:47:14 +0200

    Started working on task list

---
+++ app/components/TaskTable/TaskTable.tsx
@@ -0,0 +1,88 @@
+import { Table, Row, Col, Tooltip, User, Text } from "@nextui-org/react";
+import { TrashIcon, PencilAltIcon } from '@heroicons/react/outline'
+import { IconButton } from "./IconButton";
+import { StatusBadge } from "./StatusBadge";
+import { Tasks } from "@prisma/client";
+
+const TaskTable = (props: IProps) => {
+
+  const columns = [
+    { name: "STATUS", uid: "status" },
+    { name: "OPIS", uid: "description" },
+    { name: "ACTIONS", uid: "actions" },
+  ];
+
+  const renderCell = (task: Tasks, columnKey: any) => {
+    const cellValue = (task as any)[columnKey];
+    
+    switch (columnKey) {
+      case "status":
+        return <StatusBadge type={task.done ? 'done' : 'notDone'}>{task.done ? 'done' : 'notDone'}</StatusBadge>;
+      case "actions":
+        return (
+          <Row justify="center" align="center">
+            <Col css={{ d: "flex" }}>
+              <Tooltip content="Edit user">
+                <IconButton onClick={() => console.log("Edit user", task.id)}>
+                  <PencilAltIcon height={20} fill="#979797" />
+                </IconButton>
+              </Tooltip>
+            </Col>
+            <Col css={{ d: "flex" }}>
+              <Tooltip
+                content="Delete user"
+                color="error"
+                onClick={() => console.log("Delete user", task.id)}
+              >
+                <IconButton>
+                    <TrashIcon height={20} fill="#FF0080"/>
+                </IconButton>
+              </Tooltip>
+            </Col>
+          </Row>
+        );
+      default:
+        return cellValue;
+    }
+  };
+
+  return (
+    <Table
+      aria-label="Example table with custom cells"
+      css={{
+        height: "auto",
+        minWidth: "80%",
+        maxWidth:'80%',
+      }}
+      selectionMode="none"
+      shadow={false}
+    >
+      <Table.Header columns={columns}>
+        {(column) => (
+          <Table.Column
+            key={column.uid}
+            hideHeader={column.uid === "actions"}
+            align={column.uid === "actions" ? "center" : "start"}
+            width={column.uid === "actions" || column.uid === "status" ? "10%" : 'auto'}
+          >
+            {column.name}
+          </Table.Column>
+        )}
+      </Table.Header>
+      <Table.Body items={props.tasks}>
+        {(item: Tasks) => (
+          <Table.Row>
+            {(columnKey) => (
+              <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>
+            )}
+          </Table.Row>
+        )}
+      </Table.Body>
+    </Table>
+  );
+}
+export default TaskTable;
+
+interface IProps{
+  tasks: Tasks[]
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,13 @@
 import Header from '../components/Header'
-import { TaskView } from '../components/TaskView'
 import { getSession, useSession } from 'next-auth/react'
-import { Container, Grid, Loading, Text } from '@nextui-org/react'
+import { Container} from '@nextui-org/react'
 import AddModal from '../components/Modal/AddModal'
 import { useEffect, useState } from 'react'
 import { Tasks, TaskType } from '@prisma/client'
 import { createClient } from 'pexels'
 import prisma from '../lib/prisma'
+import TaskViewList from '../components/TaskViewList'
+import TaskTable from '../components/TaskTable/TaskTable'
 
 export async function getServerSideProps(context: any) {
   const session = await getSession(context)
@@ -78,10 +79,6 @@ const Home = ({apiKey, tasks}: any) => {
       .then((data)=>setTasksList(data))
   }
 
-  const getTasksWithType= (type: TaskType) =>{
-    return tasksList.filter(item=> item.type === type)
-  }
-
   return (
       <Container css={{w:'100%', mw:'100%', mh:'100vh', px:'0'}}>
         <AddModal 
@@ -94,37 +91,8 @@ const Home = ({apiKey, tasks}: any) => {
           openModal={() => setOpenModal(true)}
           session={session}/>
 
-        <Grid.Container gap={6} justify={'center'} alignContent='center' css={{marginInline:'auto', width:'95vw', minHeight:'calc(100vh - 72px)', maxHeight:'fit-content'}}>
-          {loading ? <Grid xs={1}>
-            <Loading color="primary" textColor="primary" size='lg'>
-              Ładowanie
-            </Loading>
-          </Grid> : null}
-          {tasksList.length === 0  && !loading ? <Grid xs={8} sm={5} lg={3} justify='center'>
-                <Text
-                      h1
-                      size={'2.5rem'}
-                      css={{
-                          textGradient: "45deg, $blue600 -20%, $pink600 50%",
-                          textAlign:'center'
-                      }}
-                      weight="bold"
-                      >
-                      Dodaj pierwsze zadanko, żeby rozpocząć :)
-                      </Text></Grid> : null}
-          {getTasksWithType(TaskType.daily).length ? <Grid xs={8} sm={5} lg={3} justify='center'>
-                <TaskView color={"45deg, $blue600 -20%, $pink600 50%"} type={TaskType.daily} tasks={getTasksWithType(TaskType.daily)}/>
-          </Grid> : null}
-          {getTasksWithType(TaskType.weekly).length ? <Grid xs={8} sm={5} lg={3} justify='center'>
-                <TaskView color={"45deg, $purple600 -20%, $pink600 100%"} type={TaskType.weekly} tasks={getTasksWithType(TaskType.weekly)}/>
-          </Grid> : null}
-          {getTasksWithType(TaskType.monthly).length ? <Grid xs={8} sm={5} lg={3} justify='center'>
-                <TaskView color={"45deg, $yellow600 -20%, $red600 100%"} type={TaskType.monthly} tasks={getTasksWithType(TaskType.monthly)}/>
-          </Grid> : null}
-          {getTasksWithType(TaskType.yearly).length ? <Grid xs={8} sm={5} lg={3} justify='center'>
-                <TaskView color={"45deg, $yellow600 -20%, $red600 100%"} type={TaskType.yearly} tasks={getTasksWithType(TaskType.yearly)}/>
-          </Grid> : null}
-        </Grid.Container>
+        {/* <TaskViewList TaskList={tasksList} loading={loading}/> */}
+        <TaskTable tasks={tasksList}/>
       </Container>
   );
 }
